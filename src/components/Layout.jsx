47ffed513da9f04dfc7c8b555e1389cfc2edbbca
Aleksei Kovalev <aleksei.kovalev412@gmail.com>
2023-01-03 08:19:50 +0400

    move to src/

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/Layout.module.scss";
+
+const Layout = ({}) => {
+  return (
+    <>
+      <Head></Head>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.scss";
+
+const Home = () => {
+  return <Layout></Layout>;
+};
+
+export default Home;
