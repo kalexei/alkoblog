82598952f81aa4c4201a155389bea53327576e4c
Aleksei Kovalev <aleksei.kovalev412@gmail.com>
2023-01-11 08:15:12 +0400

    add missing pages

---
+++ app/hireme/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hireme: React.FC = () => {
+  return <div>Hireme works!</div>;
+};
+
+export default Hireme;
---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects: React.FC = () => {
+  return <div>Projects works!</div>;
+};
+
+export default Projects;
